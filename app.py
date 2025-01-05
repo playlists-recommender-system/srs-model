1057ed8a5db48caae9e4885a02887d8cc970bc9b
Augusto <augusto.vicente@brius.com.br>
2025-01-05 17:20:33 -0300

    feat: add cors and fitter list of songs

--- app.py
+++ app.py
@@ -3,8 +3,11 @@ import pickle
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
+
+
 
 dataset_path = os.getenv("DATASET_PATH", "datasets/dataset.csv")
 app_port = os.getenv("APP_PORT")
@@ -13,6 +16,7 @@ print(f"Loading dataset from {dataset_path}.")
 playlists_df  = pd.read_csv(dataset_path, low_memory=False)
 
 app = Flask(__name__)
+CORS(app)
 
 with open('rules.pkl', 'rb') as f:
     rules = pickle.load(f)
@@ -21,8 +25,8 @@ with open('rules.pkl', 'rb') as f:
 @app.route('/recommend', methods=['POST'])
 def recommend():
     data = request.json
-    artist = data.get('artist_name')
-    song = data.get('track_name')
+    artist = data[0].get('artist_name')
+    song = data[0].get('track_name')
 
     item_uri = playlists_df[(playlists_df['artist_name'] == artist) &
                             (playlists_df['track_name'] == song)]['track_uri']
@@ -34,8 +38,11 @@ def recommend():
     recomendacoes = rules[rules['antecedents'].apply(lambda x: item_uri in x)]
 
     # Retorne as músicas recomendadas
-    recommended = [list(rule['consequents'])[0] for _, rule in recomendacoes.iterrows()]
-    return jsonify({'recommendations': recommended})
+    recommended_uris = [list(rule['consequents'])[0] for _, rule in recomendacoes.iterrows()]
+    filtered_playlists = playlists_df[playlists_df['track_uri'].isin(recommended_uris)]
+    filtered_playlists = filtered_playlists.drop_duplicates(subset='track_uri', keep='first')
+    result = filtered_playlists[['artist_name', 'track_name']].to_dict(orient='records')
+    return jsonify({'recommendations': result})
 
 if __name__ == '__main__':
     print("app_port: "+str(app_port))
